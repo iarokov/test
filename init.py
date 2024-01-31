7599868bc669e6bcf10812ced22d57d175e86c18
Ubuntu <ubuntu@ip-172-31-46-193.ec2.internal>
2024-01-31 04:47:35 +0000

    first Pyomoproject

--- init.py
+++ init.py
@@ -1,4 +1,28 @@
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
 def main():
-    x = 1
-    x = x*2
+    # Create a Pyomo model object
+    model = ConcreteModel()
+    # Define decision variables
+    model.a = Var(within=Integers)
+    model.b = Var(within=Integers)
+    # Define objective function for mximum total Profit
+    model.obj = Objective(expr = 40*model.a + 80*model.b, sense=maximize)
+    # create ConstraintList object
+    model.constraints = ConstraintList()
+
+    # Define constraints
+    model.constraints.add(expr = 2*model.a + 3*model.b <= 105) # Plant
+    model.constraints.add(expr = model.a <= 30) # For Product A
+    model.constraints.add(expr = model.b <= 20) # For product B
+    solver = SolverFactory('glpk')
+
+    # Solve the LP problem and assign output to results
+    results = solver.solve(model)
+    # Print the optimal values of the number of units for product A and Product B
+
+    print(f"Product A = {model.a.value}")
+    print(f"Product B = {model.b.value}")
+    print(f"Profit = {model.obj.expr()}")
     return 1
